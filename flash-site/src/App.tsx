aa7d093837fd124edd1a6a11e8b77a521a048062
John Karabinos <john@pantheonai.co>
2025-07-23 17:19:09 -0600

    More removal

--- flash-site/src/App.tsx
+++ flash-site/src/App.tsx
@@ -1,7 +1,6 @@
 import { PrimeReactProvider } from "primereact/api";
 import { ThemeProvider } from "./themes/Theme";
 import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
 import { Main } from "./pages/main/Main";
 
 function App() {
